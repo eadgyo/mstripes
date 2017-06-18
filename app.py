fee2ca74e39e3b72942a5601ede706bf6b80aa9f
eadgyo <eadgyo@gmail.com>
2017-06-18 13:17:08 +0530

    [UPDATE] Main view structure

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from tkinter import *
-from tktable import *
-from constants import *
+import tkinter as Tk
 from mpl_toolkits.basemap import Basemap
+from constants import *
+from tktable import *
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -9,33 +9,43 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class App:
     def __init__(self, master):
-        frame = Frame(master)
+        # Create a container for the frame
+        frame = Tk.Frame(master)
         frame.pack()
 
-        # Create the left part
-        self.loadButton = Button(frame, text=Constant.LOAD_TEXT_BUTTON)
-        self.loadButton.grid(row=0, column=0)
+        # Create the left part container
+        left_part = Tk.Frame(frame)
+        left_part.pack(side=Tk.LEFT)
 
-        self.table = Table(frame, rows=5, cols=5)
-        self.table.grid(row=2, column=0)
+        self.loadButton = Tk.Button(left_part, text=Constant.LOAD_TEXT_BUTTON)
+        self.loadButton.pack(side=Tk.TOP)
 
+        self.table = Table(left_part, rows=5, cols=5)
+        self.table.pack(side=Tk.BOTTOM, fill=Tk.BOTH, expand=1)
 
         # Create the center part
-        self.map = Basemap(llcrnrlon=-0.5,llcrnrlat=39.8,urcrnrlon=4.,urcrnrlat=43.,
-                           resolution='i', projection='tmerc', lat_0 = 39.5, lon_0 = 1, ax=self.ax1)
-        self.map.grid(row=0, column=1)
+        center_part = Tk.Frame(frame)
+        center_part.pack(side=Tk.LEFT)
 
         self.fig = Figure()
         self.ax1 = self.fig.add_subplot(111)
-        self.canvas = FigureCanvasTkAgg(self.fig, master=master)
+
+        self.map = Basemap(llcrnrlon=-0.5,llcrnrlat=39.8,urcrnrlon=4.,urcrnrlat=43.,
+                           resolution='i', projection='tmerc', lat_0 = 39.5, lon_0 = 1, ax=self.ax1)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=center_part)
         self.canvas.show()
+        self.canvas.get_tk_widget().pack(expand=1)
 
         # Create the right part
-        self.addButton = Button(frame, text=Constant.ADD_TEXT_BUTTON)
-        self.addButton.grid(row=0, column=2)
+        right_part = Tk.Frame(frame)
+        right_part.pack(side=Tk.LEFT)
+
+        self.addButton = Tk.Button(right_part, text=Constant.ADD_TEXT_BUTTON)
+        self.addButton.pack(side=Tk.TOP)
 
 
 if __name__ == '__main__':
-    root = Tk()
+    root = Tk.Tk()
     app = App(root)
     root.mainloop()
